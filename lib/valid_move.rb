d7f85d84bc73db57825a18d11e8d9c3ca060d5f9
richardtam.ny <richardtam.ny@gmail.com>
2019-01-14 21:01:04 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,11 +7,12 @@ def position_taken(array, index)?
 end
 
 def valid_move?(board, index)
-  if position_taken? == false
+  if position_taken()? == false
     if board[index] <= 8
       true
     else false
+    end
+  else false
   end
 end
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
